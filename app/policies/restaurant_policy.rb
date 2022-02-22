1dff8b8f3fca378e166fbb0c1446806675ccb54c
Jonathan Santurbano <jonathan.santurbano@gmail.com>
2022-02-22 10:14:31 +0100

    pundit is great

---
+++ app/policies/restaurant_policy.rb
@@ -0,0 +1,28 @@
+class RestaurantPolicy < ApplicationPolicy
+  class Scope < Scope
+    # NOTE: Be explicit about which records you allow access to!
+    def resolve
+      scope.all
+    end
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def show?
+    true
+  end
+
+  def update?
+    record.user == user || user.admin?
+  end
+
+  def destroy?
+    record.user == user || user.admin?
+  end
+end
